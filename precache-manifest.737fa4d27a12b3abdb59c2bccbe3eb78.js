76cadddf3baff1ddeda570e2c4b3ecf59fd50fca
anamwp <anam@themexpert.com>
2019-09-06 04:11:25 +0600

    Updates

---
+++ precache-manifest.737fa4d27a12b3abdb59c2bccbe3eb78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9b1bcd8a5fff7fe6a6ef7ead982075",
+    "url": "/dnd-demo/index.html"
+  },
+  {
+    "revision": "657ac4c7d11e279b35b9",
+    "url": "/dnd-demo/static/css/2.d5794a55.chunk.css"
+  },
+  {
+    "revision": "657ac4c7d11e279b35b9",
+    "url": "/dnd-demo/static/js/2.987f7364.chunk.js"
+  },
+  {
+    "revision": "d91a76e9bfaa31f70dce",
+    "url": "/dnd-demo/static/js/main.33d36751.chunk.js"
+  },
+  {
+    "revision": "8100e71d0547a8854b4b",
+    "url": "/dnd-demo/static/js/runtime~main.279a3728.js"
+  }
+]);
